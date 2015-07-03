60027f510b90a9c3f363c3a9b815aa9abc1646be
Martin Billinger <flkazemakase@gmail.com>
2015-07-03 17:07:51 +0200

    separated Tome:load hook function

--- addons/tome-sapper/hooks/load.lua
+++ addons/tome-sapper/hooks/load.lua
@@ -1,6 +1,8 @@
 local class = require"engine.class"
 local Birther = require "engine.Birther"
 
-class:bindHook("ToME:load", function(self, data)
+function loadHook(self, data)
 	Birther:loadDefinition("/data-sapper/birth.lua")
-end)
+end
+
+class:bindHook("ToME:load", loadHook)
