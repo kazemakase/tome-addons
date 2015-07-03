6e862db448e767361189c0b01e2bd59a73def29a
Martin Billinger <flkazemakase@gmail.com>
2015-07-03 16:37:05 +0200

    Added sapper class

---
+++ addons/tome-sapper/data/birth.lua
@@ -0,0 +1,71 @@
+
+local Particles = require "engine.Particles"
+
+newBirthDescriptor{
+	type = "subclass",
+	name = "Sapper",
+	desc = {
+		"Sappers are alchemists who like to lay traps. Or maybe they are rogues who like to dabble in magics. They certainly like to blow things up.",
+		"Their most important stats are: Magic and Cunning.",
+		"#GOLD#Stat modifiers:",
+		"#LIGHT_BLUE# * +0 Strength, +3 Dexterity, +0 Constitution",
+		"#LIGHT_BLUE# * +3 Magic, +0 Willpower, +3 Cunning",
+		"#GOLD#Life per level:#LIGHT_BLUE# +0",
+	},
+	power_source = {technique=true, arcane=true},
+	stats = { mag=3, dex=3, cun=3, },
+	birth_example_particles = {
+		function(actor)
+			actor:addShaderAura("body_of_fire", "awesomeaura", {time_factor=3500, alpha=1, flame_scale=1.1}, "particles_images/wings.png")
+		end,
+	},
+	talents_types = {
+		["spell/explosives"]={true, 0.3},
+		["spell/fire-alchemy"]={true, 0.3},
+		["spell/stone-alchemy"]={true, -0.3},
+		["spell/fire"]={false, 0.3},
+		["cunning/survival"]={false, 0.3},
+		["technique/combat-training"]={true, 0.1},
+		["technique/field-control"]={true, 0},
+		["technique/combat-techniques-passive"]={false, 0.3},
+		["technique/acrobatics"]={false, 0.3},
+		["cunning/stealth"]={true, 0.3},
+		["cunning/trapping"]={true, 0.3},
+	},
+	talents = {
+		[ActorTalents.T_CREATE_ALCHEMIST_GEMS] = 1,
+		[ActorTalents.T_THROW_BOMB] = 1,
+		[ActorTalents.T_FIRE_INFUSION] = 1,
+		[ActorTalents.T_STEALTH] = 1,
+		[ActorTalents.T_TRAP_MASTERY] = 1,
+	},
+	copy = {
+		max_life = 100,
+		mana_regen = 0.25,
+		mana_rating = 5,
+		resolvers.inscription("RUNE:_MANASURGE", {cooldown=25, dur=10, mana=620}),
+		resolvers.equipbirth{ id=true,
+			{type="weapon", subtype="dagger", name="iron dagger", autoreq=true, ego_chance=-1000},
+			{type="armor", subtype="light", name="rough leather armour", autoreq=true, ego_chance=-1000},
+			{type="armor", subtype="head", name="iron helm", autoreq=true, ego_chance=-1000},
+		},
+		resolvers.inventory{ id=true,
+			{type="gem",},
+			{type="gem",},
+			{type="gem",},
+		},
+		resolvers.generic(function(self) self:birth_create_alchemist_stuff() end),
+		birth_create_alchemist_stuff = function(self)
+			-- Make and wield some alchemist gems
+			local t = self:getTalentFromId(self.T_CREATE_ALCHEMIST_GEMS)
+			local gem = t.make_gem(self, t, "GEM_AGATE")
+			self:wearObject(gem, true, true)
+			self:sortInven()
+		end,
+	},
+	copy_add = {
+		life_rating = 0,
+	},
+}
+
+getBirthDescriptor("class", "Rogue").descriptor_choices.subclass.Sapper = "allow"
---
+++ addons/tome-sapper/init.lua
@@ -0,0 +1,33 @@
+-- ToME - Tales of Maj'Eyal:
+-- Copyright (C) 2009 - 2014 Nicolas Casalini
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Nicolas Casalini "DarkGod"
+-- darkgod@te4.org
+
+long_name = "Sapper class"
+short_name = "sapper"
+for_module = "tome"
+version = { 1, 4, 0 }
+addon_version = {0, 0, 1}
+weight = 100
+author = { "kazemakase", "" }
+homepage = "https://github.com/kazemakase"
+description = [[Adds the Sapper class to ToME.  Sappers are a mixture of Alchemists and Rogues, specializing in bombs, charges, and traps.]]
+tags = { "alchemist", "rogue", "class", "sapper" }
+overload = false
+superload = false
+hooks = true
+data = true
